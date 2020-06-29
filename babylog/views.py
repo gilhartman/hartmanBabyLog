f1771214ac4b1508a3704e789fc6061f71420270
Gil Hartman <gil@nativeflow.com>
2020-06-29 15:36:34 +0300

    change default to breastfeed

--- babylog/views.py
+++ babylog/views.py
@@ -145,7 +145,7 @@ def feed(request, baby_name):
     min_feed_amount = 10
     max_feed_amount = 200
     feed_jump_amount = 5
-    default_feed_amount = 100
+    default_feed_amount = 120
     amount = [i for i in range(min_feed_amount, max_feed_amount, feed_jump_amount)]
     background = BabyConsts.b1_background_color if baby_name == BabyConsts.b1_name else BabyConsts.b2_background_color
     return render(request, 'babylog/feed.html', locals())
